01073a4dc9ce01af5958c825db0ccb3565b888fb
Dezmon Gilbert <dog6487@rit.edu>
2017-03-31 22:29:40 -0400

    Created Enemy type classes

---
+++ ThroughTheNight/ThroughTheNight/Assets/Scripts/TankEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankEnemy : Entity {
+
+	// Use this for initialization
+	protected override void Start () {
+		speed;
+		attack;
+		health;
+		velocity;
+		direction;	
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		
+	}
+
+	//method to spawn entity into the game
+	protected override void Spawn(Vector3 location, Vector3 rotation){
+	}
+
+	//method to move the entity
+	protected override void Move(){
+		
+	}
+
+	//method to handle when the entity dies
+	protected override void Death(){
+		
+	}
+
+	//method to handle when the entity is attacked
+	protected override void Damaged(){
+		
+	}
+
+	//method to handle when the entity attacks
+	protected override void Attack(){
+		
+	}
+}
