1c81e118acd1e2aa1bdd1856aebc9b94d4753604
James Troup (RIT Student) <jlt9768@ad.rit.edu>
2017-04-01 10:32:51 -0400

    GameManager States and beginning of objectives

--- ThroughTheNight/ThroughTheNight/Assets/Scripts/GameManager.cs
+++ ThroughTheNight/ThroughTheNight/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
+    public enum State { Day, Buy, Night } 
 
     public static GameManager GM;
 
@@ -13,6 +14,17 @@ public class GameManager : MonoBehaviour {
     public Text time;
     public double timeNum;
 
+    public Text remaining;
+    public int remainNum;
+
+    public Text objective;
+
+    public State currentState;
+
+    public Text[] textElements;
+    public Font[] fonts;
+    public List<string> objectives = new List<string>();
+    
     void Awake()
     {
         CreateGameManager();
@@ -37,23 +49,59 @@ public class GameManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        objective = GameObject.Find("Objectives").GetComponent<Text>();
+        objectives = new List<string>();
+        objectives.Add("Cook");
+        objectives.Add("Clean");
+        objectives.Add("Shower");
+
+        
         health = GameObject.Find("Health").GetComponent<Text>();
         health.text = "Health: " + healthNum;
 
         time = GameObject.Find("Time").GetComponent<Text>();
         time.text = "Time: " + timeNum;
+
+        remaining = GameObject.Find("Remaining").GetComponent<Text>();
+        remaining.text = remainNum.ToString();
+
+        for(int i = 0; i < objectives.Count; i++)
+        {
+            objective.text += "     " + objectives[i] + "\n";
+        }
+
+        UpdateObjective("Cook");
+
+        //Starting at Day
+        ChangeState(State.Day);
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        timeNum -= Time.deltaTime;
-        time.text = "Time: " + timeNum.ToString("F2");
+        if(currentState == State.Night)
+        {
+            timeNum -= Time.deltaTime;
+            time.text = "Time: " + timeNum.ToString("F2");
+        }
+       
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if(currentState == State.Night)
+            {
+                ChangeState(State.Day);
+            }else if(currentState == State.Day)
+            {
+                ChangeState(State.Night);
+            }
+            
+        }
     }
 
     /// <summary>
     /// Change the health of the player. Use negatives to decrease health
     /// </summary>
-    /// <param name="health">Amount of health the player loses</param>
+    /// <param name="health">Amount of health the player loses/gains </param>
     public void ChangeHealth(int health)
     {
         if(health == 0)//if input is zero set health to zero instead of changing it by zero
@@ -66,4 +114,61 @@ public class GameManager : MonoBehaviour {
         }
         GameManager.GM.health.text = "Health: " + GameManager.GM.healthNum;
     }
+    
+    /// <summary>
+    /// Called to decrease remaining enemy count once and enemy has been destroyed
+    /// </summary>
+    public void DefeatEnemy()
+    {
+        remainNum--;
+        remaining.text = remainNum.ToString();
+    }
+
+
+    /// <summary>
+    /// Change the game between it's seperate states
+    /// </summary>
+    /// <param name="change">The state to change the game to</param>
+    void ChangeState(State change)
+    {
+        currentState = change;
+        switch (currentState)
+        {
+            case State.Day:
+                { 
+                    for(int i = 0; i < textElements.Length; i++)
+                    {
+                        textElements[i].GetComponent<Text>().font = fonts[1];
+                    }                   
+                    Camera.main.backgroundColor = new Color(155, 155, 155);
+                    remaining.text = "0";
+                    return;
+                }
+            case State.Night:
+                {
+                    for (int i = 0; i < textElements.Length; i++)
+                    {
+                        textElements[i].GetComponent<Text>().font = fonts[0];                        
+                    }
+                    Camera.main.backgroundColor = new Color(125, 0, 0);
+                    remaining.text = remainNum.ToString();
+                    return;
+                }
+            case State.Buy:
+                {
+                    return;
+                }
+        }
+
+    }
+    void UpdateObjective(string obj)
+    {
+        objectives.Remove(obj);
+        objective.text = "Objectives: \n";
+        for (int i = 0; i < objectives.Count; i++)
+        {
+            objective.text += "     " + objectives[i] + "\n";
+        }
+    }
+
 }
